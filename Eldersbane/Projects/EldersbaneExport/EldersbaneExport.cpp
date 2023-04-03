8858fb6fbc9cf32029ce9ae4ae55017f505b2d49
elisatodd <elitodd@ucm.es>
2023-04-03 18:51:04 +0200

    Refactorización de carpetas
    Co-Authored-By: Miguel González Pérez <82326243+miggon23@users.noreply.github.com>

---
+++ Eldersbane/Projects/EldersbaneExport/EldersbaneExport.cpp
@@ -0,0 +1,15 @@
+// EldersbaneExport.cpp : Define las funciones exportadas del archivo DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "EldersbaneExport.h"
+
+#include <iostream>
+
+bool InitJuego(void)
+{
+	//CMotorDll* miMotor = CMotor::Instance();
+	//miMotor->HazAlgo();
+	std::cout << "Fin juego \n";
+}
---
+++ Eldersbane/Projects/EldersbaneExport/EldersbaneExport.h
@@ -0,0 +1,13 @@
+// El siguiente bloque ifdef muestra la forma estándar de crear macros que hacen la exportación
+// de un DLL más sencillo. Todos los archivos de este archivo DLL se compilan con ELDERSBANEEXPORT_EXPORTS
+// símbolo definido en la línea de comandos. Este símbolo no debe definirse en ningún proyecto
+// que use este archivo DLL. De este modo, otros proyectos cuyos archivos de código fuente incluyan el archivo verán
+// interpretan que las funciones ELDERSBANEEXPORT_API se importan de un archivo DLL, mientras que este archivo DLL interpreta los símbolos
+// definidos en esta macro como si fueran exportados.
+#ifdef ELDERSBANEEXPORT_EXPORTS
+#define ELDERSBANEEXPORT_API __declspec(dllexport)
+#else
+#define ELDERSBANEEXPORT_API __declspec(dllimport)
+#endif
+
+extern ELDERSBANEEXPORT_API bool InitJuego(void);
