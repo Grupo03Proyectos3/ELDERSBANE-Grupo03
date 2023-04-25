40859ff95873ff550492c863528e1c9388192ba7
elisatodd <elitodd@ucm.es>
2023-04-26 01:27:40 +0200

    Update BuildGame.bat
    Solo falta ejecutar correctamente el exe del juego

--- Eldersbane/BuildGame.bat
+++ Eldersbane/BuildGame.bat
@@ -1,14 +1,13 @@
-set EXES=.\Motor-Grupo3\Exes
+set EXES=.\Motor-Grupo3\Exes\
+set GAME_EXES=.\Exes\Assets\
 
 :: Compilar la solucion del juego para generar las DLLs
 msbuild "Eldersbane.sln" /p:configuration=Debug
 msbuild "Eldersbane.sln" /p:configuration=Release
 
-:: Renombrar DLLs para que sean genéricas
-
 :: Copia de DLLs a Exes 
-XCOPY /y /s .\Bin\EldersbaneExport.dll %EXES%
-XCOPY /y /s .\Bin\EldersbaneExport_d.dll %EXES%
+XCOPY /y /s .\Bin\GameExport.dll %EXES%
+XCOPY /y /s .\Bin\GameExport_d.dll %EXES%
 
 :: Copiar la carpeta de Assets -> NO
 :: XCOPY /y /s .\Exes\Assets %EXES%
@@ -18,6 +17,21 @@ cd .\Motor-Grupo3\
 msbuild "MiMotor.sln" /p:configuration=Debug
 msbuild "MiMotor.sln" /p:configuration=Release
 
+cd ..
+
 :: Mover assets del motor y .exe del motor a Eldersbane
+XCOPY /y /s %EXES% %GAME_EXES%
+::XCOPY /y /s %EXES%Motor.exe .\Exes\
+::XCOPY /y /s %EXES%Motor_d.exe .\Exes\
 
 :: Ejecutar .exe del motor
+
+::@echo off
+::start /wait "engine" ".\Exes\Motor_d.exe"
+::echo "engine has finished running."
+
+::".\Exes\Assets\Motor.exe" ".\Exes\Assets\resources.cfg"
+call ".\Exes\Assets\Motor.exe"
+
+::cmd /c ".\Exes\Motor_d.exe"
+
