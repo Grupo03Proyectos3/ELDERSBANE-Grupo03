3d8021faf3d7359b166ce463b04c55a7db8c4771
AntonioPove <79278677+AntonioPove@users.noreply.github.com>
2023-05-05 03:23:47 +0200

    LoseGame mapa

--- Eldersbane/Exes/Assets/Scripts/LoseGame.lua
+++ Eldersbane/Exes/Assets/Scripts/LoseGame.lua
@@ -1,54 +1,6 @@
-entities = {"Background", "MenuButton", "PlayButton", "CamaraPpal", "Luz"}
-Background = {
-    Name={
-        t_name="backgroundLose"
-    },
-    Transform = {
-        positionX = "39.0",
-        positionY = "55.0",
-        positionZ = "0.0",
-        scaleX = "130.0",
-        scaleY = "100.0",
-        scaleZ = "1.0",
-        rotationX = "0.0",
-        rotationY = "0.0",
-        rotationZ = "0.0",
-        rotationW = "1.0",
-    },
-    UIElement = {
-        t_type = "FlamingoDefaultUI/StaticImage",
-        t_name = "backgroundLose",
-        t_text = "",
-        t_image = "EldersbaneEndGame.png",
-    },
-}
-PlayButton = {
-    Name={
-        t_name="playButtonLose"
-    },
-    Transform = {
-        positionX = "39.0",
-        positionY = "55.0",
-        positionZ = "0.0",
-        scaleX = "130.0",
-        scaleY = "100.0",
-        scaleZ = "1.0",
-        rotationX = "0.0",
-        rotationY = "0.0",
-        rotationZ = "0.0",
-        rotationW = "1.0"
-    },
-    UIElement = {
-        t_type = "FlamingoDefaultUI/ImageButton",
-        t_name = "playButtonLose",
-        t_text = "",
-        t_image = "Play.png"
-    },
-    Scripts = {
-        t_scriptName = "GoToGame"
-    },
-}
-MenuButton = {
+entities = {"PlayGame", "MenuGame", "Back", "CamaraPpal", "Luz"}
+
+MenuGame = {
     Name={
         t_name="menuButtonLose"
     },
@@ -71,12 +23,62 @@ MenuButton = {
         t_image = "Menu.png"
     },
     Scripts = {
-        t_scriptName = "GoToMenu"
+        "GoToMenu"
+    }
+}
+Back = {
+    Name={
+        t_name="end"
     },
+    Transform = {
+        positionX = "0.0",
+        positionY = "0.0",
+        positionZ = "0.0",
+        scaleX = "800.0",
+        scaleY = "800.0",
+        scaleZ = "1.0",
+        rotationX = "0.0",
+        rotationY = "0.0",
+        rotationZ = "0.0",
+        rotationW = "1.0"
+    },
+    UIElement = {
+        t_type = "FlamingoDefaultUI/StaticImage",
+        t_name = "end",
+        t_text = "",
+        t_image = "EldersbaneEndGame.png"
+    }
+}
+
+PlayGame = {
+    Name={
+        t_name="playButtonLose"
+    },
+    Transform = {
+        positionX = "39.0",
+        positionY = "55.0",
+        positionZ = "0.0",
+        scaleX = "130.0",
+        scaleY = "100.0",
+        scaleZ = "1.0",
+        rotationX = "0.0",
+        rotationY = "0.0",
+        rotationZ = "0.0",
+        rotationW = "1.0"
+    },
+    UIElement = {
+        t_type = "FlamingoDefaultUI/ImageButton",
+        t_name = "playButtonLose",
+        t_text = "",
+        t_image = "Play.png"
+    },
+    Scripts = {
+       "GoToGame"
+    }
 }
 CamaraPpal={
     Name={
-        t_name="myCamera"
+        t_name="myCameraend"
     }, --Para una vista cenital la posicion sera (0 48000 0)
     Transform = {
         positionX = "500.0",
@@ -91,8 +93,8 @@ CamaraPpal={
         rotationW = "1.0"
     },
     Camera={
-        t_name="myCamera",
-        t_entity_name="myCamera2",
+        t_name="myCameraend",
+        t_entity_name="myCamera2end",
         t_color_x="0.1",
         t_color_y="0.4",
         t_color_z="0.7"
@@ -100,7 +102,7 @@ CamaraPpal={
 }
 Luz={
     Name={
-        t_name="mylight"
+        t_name="mylightEnd"
     },
     Transform = {
         positionX = "500.0",
@@ -115,6 +117,6 @@ Luz={
         rotationW = "1.0"
     },
     Light={
-        t_name="myLight2"
+        t_name="myLightend"
     }
 }
