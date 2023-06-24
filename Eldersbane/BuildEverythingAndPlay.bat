27247a90fe191ae5b426aefd086f630a070108a8
ssimoanto <simoanto@ucm.es>
2023-06-24 19:47:47 +0200

    Update BuildEverythingAndPlay.bat
    Co-Authored-By: Adrián Montero <82326212+admont02@users.noreply.github.com>

--- Eldersbane/BuildEverythingAndPlay.bat
+++ Eldersbane/BuildEverythingAndPlay.bat
@@ -8,24 +8,4 @@ cd .\Motor-Grupo3\
 call BuildEngine.bat
 cd ..
 
-:: Compilar la solucion del juego para generar las DLLs
-msbuild "Eldersbane.sln" /p:configuration=Debug
-msbuild "Eldersbane.sln" /p:configuration=Release
-
-:: Copia de DLLs a Exes 
-XCOPY /y /s Bin\GameExport.dll %EXES%
-XCOPY /y /s Bin\GameExport_d.dll %EXES%
-
-:: Compila la solucion del motor
-cd .\Motor-Grupo3\
-msbuild "MiMotor.sln" /p:configuration=Debug
-msbuild "MiMotor.sln" /p:configuration=Release
-
-cd ..
-
-:: Mover assets del motor y .exe del motor a Eldersbane
-XCOPY /y /s .\Motor-Grupo3\Exes .\Exes
-
-@echo off
-cd .\Exes\
-call Motor.exe
+call BuildGameAndPlay.bat
